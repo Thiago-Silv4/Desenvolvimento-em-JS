1e146327016694094a57494ab2b14daacd0b80e4
Thiago-Silv4 <thgsilvaaa2000@gmail.com>
2025-08-01 17:57:59 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,24 @@
+// Comentario de linha
+/*
+ pular linha
+*/
+
+// Estrutura de dado
+
+var numero = 10
+let numero2 = 20
+const numero3 = 30
+
+let idade = 25
+let peso = 65.12
+let x = true
+let texto = 'Isso é um texto'
+
+console.log(peso)
+
+// tipos de dados
+
+//real (float) - 1.2 3.0 4.5 - dinheiro peso altura...
+//boleano (bool) - true false 0/1 - condições 
+//inteiro (int) - -1   0    1 - altura - ano - nº filhos...
+//texto (string) - 'Isso é um texto' "Isso tambem é um texto" - msg , label's...
