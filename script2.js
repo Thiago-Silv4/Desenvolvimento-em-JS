5ecd29c06ed9c67c319b4f19b06d9c2640098e93
Thiago-Silv4 <thgsilvaaa2000@gmail.com>
2025-08-07 17:46:24 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,24 +1,80 @@
-// Comentario de linha
-/*
- pular linha
-*/
+// ATIVIDADE 1
 
-// Estrutura de dado
+// let numero = parseFloat(prompt("Digite um número:"));
 
-var numero = 10
-let numero2 = 20
-const numero3 = 30
+// if (isNaN(numero)) {
+//   alert("Isso não é um número válido.");
+// } else if (numero > 0) {
+//   alert("O número é positivo.");
+// } else if (numero < 0) {
+//   alert("O número é negativo.");
+// } else {
+//   alert("O número é zero.");
+// }
 
-let idade = 25
-let peso = 65.12
-let x = true
-let texto = 'Isso é um texto'
+// ATIVIDADE 2
 
-console.log(peso)
+// let numero = prompt('Digite um número inteiro');
+// if (numero % 2 === 0) {
+//     console.log('O numero é par');
+// } else {
+//     console.logo('O numero é par');
+// }
 
-// tipos de dados
+// ATIVIDADE 3
 
-//real (float) - 1.2 3.0 4.5 - dinheiro peso altura...
-//boleano (bool) - true false 0/1 - condições 
-//inteiro (int) - -1   0    1 - altura - ano - nº filhos...
-//texto (string) - 'Isso é um texto' "Isso tambem é um texto" - msg , label's...
+// let numero = 7; 
+
+// if (numero % 2 === 0) {
+//   console.log(`O número ${numero} é par.`);
+// } else {
+//   console.log(`O número ${numero} é ímpar.`);
+// }
+
+
+// ATIVIDADE 4
+
+// let lado1 = parseFloat(prompt('Digite um nome de triangulo'));
+// let lado2 = parseFloat(prompt('Digite um nome de triangulo'));
+// let lado3 = parseFloat(prompt('Digite um nome de triangulo'));
+
+// if(lado1==lado2==lado3==lado1){
+//     console.log('Equilatero');
+// } 
+// else if (lado1==lado2==lado3==lado1){
+//     console.log('Isosceles');
+// }
+// else{
+//     console.log('Escaleno');
+// }
+    
+// ATIVIDADE 4 
+
+// let numero = 10; 
+
+// if (numero % 5 === 0 && numero % 7 === 0) {
+//   console.log(`O número ${numero} é múltiplo de 5 e 7 simultaneamente.`);
+// } else {
+//   console.log(`O número ${numero} não é múltiplo de 5 e 7 simultaneamente.`);
+// }
+
+// ATIVIDADE 5
+
+// let numero = 30; // 
+
+// if (numero > 0 && numero > 15) {
+//   console.log(`O número ${numero} é positivo e maior que 15.`);
+// } else {
+//   console.log(`O número ${numero} não atende às duas condições.`);
+// }
+
+// ATIVIDADE 6 
+
+// let numero = 18;
+
+// if (numero % 9 === 0 && numero % 6 === 0) {
+//   console.log(`O número ${numero} é divisível por 9 e 6 simultaneamente.`);
+// } else {
+//   console.log(`O número ${numero} não é divisível por 9 e 6 simultaneamente.`);
+// }
+
