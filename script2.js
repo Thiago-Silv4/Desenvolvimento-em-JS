c69b028999ec8643de71acce9f2ca90234d94026
Thiago-Silv4 <thgsilvaaa2000@gmail.com>
2025-08-08 17:40:01 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,80 +1,96 @@
 // ATIVIDADE 1
 
-// let numero = parseFloat(prompt("Digite um número:"));
+// while (Loop com Condição)
 
-// if (isNaN(numero)) {
-//   alert("Isso não é um número válido.");
-// } else if (numero > 0) {
-//   alert("O número é positivo.");
-// } else if (numero < 0) {
-//   alert("O número é negativo.");
-// } else {
-//   alert("O número é zero.");
+// let contador = 0;
+// while (contador < 11) {
+// console.log(`Contagem: ${contador}`);
+// contador++;
 // }
 
 // ATIVIDADE 2
 
-// let numero = prompt('Digite um número inteiro');
-// if (numero % 2 === 0) {
-//     console.log('O numero é par');
-// } else {
-//     console.logo('O numero é par');
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
 // }
 
 // ATIVIDADE 3
 
-// let numero = 7; 
+// const numero = 5;
 
-// if (numero % 2 === 0) {
-//   console.log(`O número ${numero} é par.`);
-// } else {
-//   console.log(`O número ${numero} é ímpar.`);
+// for (let i = 1; i <= 10; i++) {
+//     const resultado = numero * i;
+//     console.log(`${numero} x ${i} = ${resultado}`);
 // }
-
 
 // ATIVIDADE 4
 
-// let lado1 = parseFloat(prompt('Digite um nome de triangulo'));
-// let lado2 = parseFloat(prompt('Digite um nome de triangulo'));
-// let lado3 = parseFloat(prompt('Digite um nome de triangulo'));
-
-// if(lado1==lado2==lado3==lado1){
-//     console.log('Equilatero');
-// } 
-// else if (lado1==lado2==lado3==lado1){
-//     console.log('Isosceles');
-// }
-// else{
-//     console.log('Escaleno');
-// }
-    
-// ATIVIDADE 4 
-
-// let numero = 10; 
-
-// if (numero % 5 === 0 && numero % 7 === 0) {
-//   console.log(`O número ${numero} é múltiplo de 5 e 7 simultaneamente.`);
-// } else {
-//   console.log(`O número ${numero} não é múltiplo de 5 e 7 simultaneamente.`);
+// const carros = ['Audi', 'BMW', 'GOL']
+// for (const carro of carros){
+//     console.log(carro);
 // }
 
 // ATIVIDADE 5
 
-// let numero = 30; // 
+// let soma = 0;
+// for (let i = 1; i <= 100; i++) {
+//   soma = soma + i;
+// }
+// console.log(`A soma dos números de 1 a 100 é: ${soma}`);
 
-// if (numero > 0 && numero > 15) {
-//   console.log(`O número ${numero} é positivo e maior que 15.`);
-// } else {
-//   console.log(`O número ${numero} não atende às duas condições.`);
+// ATIVIDADE 6
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+// console.log("Thiago passou por aqui!"); 
+
+// ATIVIDADE 7 
+
+// const numeroSecreto = Math.floor(Math.random() * 10) + 1;
+
+// let tentativas = 0;
+// let palpiteUsuario = 0;
+
+// while (palpiteUsuario !== numeroSecreto) {
+
+//   palpiteUsuario = parseInt(prompt("Adivinhe o número secreto (1 a 10):"));
+
+//   tentativas++;
+
+//   if (palpiteUsuario === numeroSecreto) {
+//     alert(`Parabéns! Você acertou em ${tentativas} tentativa(s)! O número secreto era ${numeroSecreto}.`);
+//   } else if (palpiteUsuario < numeroSecreto) {
+//     alert("O número secreto é maior! Tente novamente.");
+//   } else {
+//     alert("O número secreto é menor! Tente novamente.");
+//   }
 // }
 
-// ATIVIDADE 6 
+// ATIVIDADE 8
 
-// let numero = 18;
+// let soma = 0;
+// let numero;
 
-// if (numero % 9 === 0 && numero % 6 === 0) {
-//   console.log(`O número ${numero} é divisível por 9 e 6 simultaneamente.`);
-// } else {
-//   console.log(`O número ${numero} não é divisível por 9 e 6 simultaneamente.`);
+// do {
+//   numero = parseInt(prompt("Digite um número para somar (ou 0 para parar):"));
+  
+//   if (numero !== 0) {
+//     soma += numero;
+//   }
+  
+// } while (numero !== 0);
+
+// alert(`A soma total é: ${soma}`);
+
+// ATIVIDADE 9
+
+// const frutas = ['Maçã', 'Banana', 'Laranja', 'Pera'];
+// let i = 0; 
+
+// while (i < frutas.length) {
+//     console.log(frutas[i]);
+//     i++; 
 // }
-
