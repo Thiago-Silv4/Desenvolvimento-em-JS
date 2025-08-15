27ec12149eec25da0ec86e6af6ff38242fadb2e5
Thiago-Silv4 <thgsilvaaa2000@gmail.com>
2025-08-15 17:58:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,13 @@
- // SISTEMA DE ATENDIMENTO AO CLIENTE
- 
-// let nomeCliente1 = "João";
-// let numPessoas1 = 2;
-// let tipoMesa1 = "Padrão";
+function openNav(){
 
-// let nomeCliente2 = "Maria";
-// let numPessoas2 = 1;
-// let tipoMesa2 = "VIP";
-
-// let nomeCliente3 = "Thiago";
-// let numPessoas3 = 3;
-// let tipoMesa3 = "Varanda";
-
-// let consumoMinimoPadrao = 50;
-// let consumoMinimoVIP = 80;
-// let consumoMinimoVaranda = 100;
-
-// let consumoMinimoCliente1, consumoMinimoCliente2, consumoMinimoCliente3;
+  let x = document.getElementById('navigation')
+  if (x.className === 'navigation'){
+      x.className += " menujs";
+      document.getElementById('treeline-icon').innerHTML = "&Cross";
+  }else{
+    x.className = 'navigation';
+    document.getElementById('treeline-icon').innerHTML = "&#9776;";
+  }
 
 
-// if (tipoMesa1 === "Padrão") {
-//   consumoMinimoCliente1 = consumoMinimoPadrao;
-// } else if (tipoMesa1 === "VIP") {
-//   consumoMinimoCliente1 = consumoMinimoVIP;
-// } else {
-//   consumoMinimoCliente1 = consumoMinimoVaranda;
-// }
-
-// if (tipoMesa2 === "Padrão") {
-//   consumoMinimoCliente2 = consumoMinimoPadrao;
-// } else if (tipoMesa2 === "VIP") {
-//   consumoMinimoCliente2 = consumoMinimoVIP;
-// } else {
-//   consumoMinimoCliente2 = consumoMinimoVaranda;
-// }
-
-// if (tipoMesa3 === "Padrão") {
-//   consumoMinimoCliente3 = consumoMinimoPadrao;
-// } else if (tipoMesa3 === "VIP") {
-//   consumoMinimoCliente3 = consumoMinimoVIP;
-// } else {
-//   consumoMinimoCliente3 = consumoMinimoVaranda;
-// }
-
-// let valorTotal1 = consumoMinimoCliente1 * numPessoas1;
-// let valorTotal2 = consumoMinimoCliente2 * numPessoas2;
-// let valorTotal3 = consumoMinimoCliente3 * numPessoas3;
-
-// console.log(`--- Resumo do Pedido ---`);
-// console.log(`Consumo Mínimo do Restaurante:`);
-// console.log(`- Mesa Padrão: R$ ${consumoMinimoPadrao}`);
-// console.log(`- Mesa VIP: R$ ${consumoMinimoVIP}`);
-// console.log(`- Mesa Varanda: R$ ${consumoMinimoVaranda}`);
-// console.log(` `);
-// console.log(`Resumo por Cliente:`);
-// console.log(`- Cliente: ${nomeCliente1}, Mesa: ${tipoMesa1}, Pessoas: ${numPessoas1}, Valor Total: R$ ${valorTotal1}`);
-// console.log(`- Cliente: ${nomeCliente2}, Mesa: ${tipoMesa2}, Pessoas: ${numPessoas2}, Valor Total: R$ ${valorTotal2}`);
-// console.log(`- Cliente: ${nomeCliente3}, Mesa: ${tipoMesa3}, Pessoas: ${numPessoas3}, Valor Total: R$ ${valorTotal3}`);
-
+}
