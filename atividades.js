beeafe5a2fe6aacc40f76ee47046b1022fcdd02f
Thiago-Silv4 <thgsilvaaa2000@gmail.com>
2025-08-20 17:42:50 -0300

    Add files via upload

---
+++ atividades.js
@@ -0,0 +1,94 @@
+// Atividade 1
+
+// class Pessoa{
+//     constructor(){ // auxiliador da class
+//     this.nome = 'Maria';
+//     this.idade = '22';
+//     this.endereco = 'Rua 10, bairro'
+//     this.formacao = 'JS'
+   
+//     }
+// }
+    
+//     const pessoa1 = new Pessoa('Maria', 10, 'Rua,10 bairro', 'JS');
+
+//     console.log(pessoa1.nome);
+//     console.log(pessoa1.idade);
+//     console.log(pessoa1.endereco);
+//     console.log(pessoa1.formacao);
+
+// Atividade 2
+
+// class Formas{
+//     constructor(){
+//         this.largura = 60;
+//         this.altura = 3;
+//     }
+// }
+
+// const formas1 = new Formas();
+
+// console.log(formas1.largura)
+// console.log(formas1.altura)
+
+// Atividade 3
+
+// class ContaBancaria {
+//   constructor(titular) {
+//     this.titular = titular;
+//     this.saldo = 0; // O saldo inicial é sempre zero.
+//   }
+
+//   depositar(valor) {
+//     if (valor > 0) {
+//       this.saldo += valor;
+//       console.log(`Depósito de R$${valor.toFixed(2)} realizado com sucesso.`);
+//       this.mostrarSaldo();
+//     } else {
+//       console.log('Erro: O valor do depósito deve ser maior que zero.');
+//     }
+//   }
+
+//   sacar(valor) {
+//     if (valor > 0 && valor <= this.saldo) {
+//       this.saldo -= valor;
+//       console.log(`Saque de R$${valor.toFixed(2)} realizado com sucesso.`);
+//       this.mostrarSaldo();
+//     } else if (valor > this.saldo) {
+//       console.log('Erro: Saldo insuficiente.');
+//     } else {
+//       console.log('Erro: O valor do saque deve ser maior que zero.');
+//     }
+//   }
+
+//   mostrarSaldo() {
+//     console.log(`Saldo atual da conta de ${this.titular}: R$${this.saldo.toFixed(2)}`);
+//   }
+// }
+
+
+// const minhaConta = new ContaBancaria('Joana');
+
+// // 2. Realizando operações
+// minhaConta.depositar(500);
+// minhaConta.sacar(150);     
+// minhaConta.sacar(400);    
+// minhaConta.depositar(200); 
+// minhaConta.sacar(550);     
+
+// Atividade 4
+
+// class Produto{
+//     constructor(){
+//         this.cor = 'Laranja';
+//         this.tamanho = 3;
+//     }
+// }
+
+// const produto1 = new Produto('Laranja', 3)
+
+// console.log(produto1.cor)
+// console.log(produto1.tamanho)
+
+// Atividade 5
+
