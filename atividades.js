a6feb0fb970d418b318171bbe4df99de3d658039
Thiago-Silv4 <thgsilvaaa2000@gmail.com>
2025-08-21 14:47:49 -0300

    Update atividades.js

--- atividades.js
+++ atividades.js
@@ -1,98 +1,148 @@
-// Atividade 1
+// // 1:  Classe Pessoa Básica -atributos  -  nome, idade, endereco, formacao
 
-// class Pessoa{
-//     constructor(){ // auxiliador da class
-//     this.nome = 'Maria';
-//     this.idade = '22';
-//     this.endereco = 'Rua 10, bairro'
-//     this.formacao = 'JS'
-   
-//     }
-// }
-    
-//     const pessoa1 = new Pessoa('Maria', 10, 'Rua,10 bairro', 'JS');
 
-//     console.log(pessoa1.nome);
-//     console.log(pessoa1.idade);
-//     console.log(pessoa1.endereco);
-//     console.log(pessoa1.formacao);
+// class Pessoa {
+//       constructor(){
+//       this.nome = 'Carlos';
+//       this.idade = 25;
+//       this.formacao = 'ADS';
 
-// Atividade 2
+//       }
 
-// class Formas{
-//     constructor(){
-//         this.largura = 60;
-//         this.altura = 3;
-//     }
+//       display(){
+//         console.log(this.nome, this.idade, this.formacao)
+//       }
+
 // }
 
-// const formas1 = new Formas();
+// const nova_pessoa =  new Pessoa()
 
-// console.log(formas1.largura)
-// console.log(formas1.altura)
 
-// Atividade 3
+// // 2: Classe Retângulo - atributos -  largura e altura
 
-// class ContaBancaria {
-//   constructor(titular) {
-//     this.titular = titular;
-//     this.saldo = 0; // O saldo inicial é sempre zero.
-//   }
+// class Retangulo{
+//       constructor(){
+//       this.largura =  20;
+//       this.altura = 150 
+       
 
-//   depositar(valor) {
-//     if (valor > 0) {
-//       this.saldo += valor;
-//       console.log(`Depósito de R$${valor.toFixed(2)} realizado com sucesso.`);
-//       this.mostrarSaldo();
-//     } else {
-//       console.log('Erro: O valor do depósito deve ser maior que zero.');
-//     }
-//   }
 
-//   sacar(valor) {
-//     if (valor > 0 && valor <= this.saldo) {
-//       this.saldo -= valor;
-//       console.log(`Saque de R$${valor.toFixed(2)} realizado com sucesso.`);
-//       this.mostrarSaldo();
-//     } else if (valor > this.saldo) {
-//       console.log('Erro: Saldo insuficiente.');
-//     } else {
-//       console.log('Erro: O valor do saque deve ser maior que zero.');
-//     }
-//   }
+//       }
+     
+//       display(){
+//         console.log(this.altura, this.altura)
+//       }
 
-//   mostrarSaldo() {
-//     console.log(`Saldo atual da conta de ${this.titular}: R$${this.saldo.toFixed(2)}`);
-//   }
+
 // }
 
 
-// const minhaConta = new ContaBancaria('Joana');
+// novo_retangulo = new Retangulo()
 
-// // 2. Realizando operações
-// minhaConta.depositar(500);
-// minhaConta.sacar(150);     
-// minhaConta.sacar(400);    
-// minhaConta.depositar(200); 
-// minhaConta.sacar(550);     
 
-// Atividade 4
+
+// // 3: Classe Conta Bancária , métodos - saque, deposito
+
+
+
+// class ContaBancaria{
+//       constructor(saldo){
+//        this.saldo = saldo
+//       }
+      
+//       display(){
+
+//         console.log(' Saldo  -  R$', this.saldo)
+//       }
+
+//       saque(valor_){
+//        let calculo = this.saldo - valor_
+//        return calculo
+//       }
+      
+//        deposito(valor_){
+//        let calculo = this.saldo + valor_
+//        return calculo
+//       }
+
+// }
+
+
+// let conta = new ContaBancaria(1000)
+
+// conta.display()
+// let v_saque  =  parseFloat(prompt('Digite o valor do saque'))
+// console.log('Valor em conta R$', conta.saque(v_saque))
+// let v_dep  =  parseFloat(prompt('Digite o valor do deposito'))
+// console.log('Valor em conta R$', conta.deposito(v_dep))
+
+
+
+
+
+
+// // 4: Classe Produto - attributos -  cor, tamanho
+
 
 // class Produto{
 //     constructor(){
-//         this.cor = 'Laranja';
-//         this.tamanho = 3;
+
+//      this.cor  = 'red';
+//      this.tamanho =  65;
+
 //     }
+
+
+
+
 // }
 
-// const produto1 = new Produto('Laranja', 3)
-
-// console.log(produto1.cor)
-// console.log(produto1.tamanho)
-
-// Atividade 5
-
-Air Supply - Making Love Out Of Nothing At All (Official HD Video)
-Bonnie Tyler - Total Eclipse of the Heart (Turn Around) (Official Video)
 
 
+
+// 5: Classe Carro -  método dirigir
+
+
+// class Carro{
+//     constructor(velocidade){
+//     this.velocidade = velocidade
+//     }
+    
+//      dirigir(){
+//      let acel = [40,50,60,70,80,90,100,150]   
+//      if (this.velocidade == 0){
+//         for (let x = 0; acel.length; x++){
+//             this.velocidade = this.velocidade + acel
+//             console.log(this.velocidade)
+//      }
+     
+//     }
+//    }
+// }
+
+// let carro =  new  Carro(0) 
+// carro.dirigir()
+
+
+
+// 75 Classe Aluno -  atributos - nome, serie - método estudar()
+
+class Aluno{
+
+      constructor(){
+           this.nome = 'Pedro';
+           this.serie = '8ª série'
+   
+      }
+      estudar(metodologia){
+      if (metodologia == 'em grupo' || metodologia == 'escrever com caneta'|| metodologia == 'criar resumo'  ){
+        console.log('Basta estudar apenas 1h dia')
+      }else{
+         console.log('estudar ao menos 2h o dia ')
+      }  
+    } 
+}
+
+
+let aluno  = new Aluno()
+aluno.estudar('em grupo')
